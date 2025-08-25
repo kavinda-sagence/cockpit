23d7e66ed7c6cf14ca61209718ee8dc3aaaa13d2
avinda-sagence <kravishan@sagence-ai.com>
2025-08-25 16:48:48 +0530

    custom bin added as cmd

--- pkg/long-running-process/index.js
+++ pkg/long-running-process/index.js
@@ -6,7 +6,7 @@ import { LongRunningProcess, ProcessState } from './long-running-process.js';
 let state, command, run_button, output, clear_button;
 
 // default shell command for the long-running process to run
-const default_command = "echo \"User: $(whoami), WD: $(pwd), Starting at: $(date)\"; for i in $(seq 10); do echo \"$i\"; sleep 1; done; echo \"Finished at: $(date)\"";
+const default_command = "/home/kavinda/Desktop/MySpace/code/example/run.sh";
 
 // follow live output of the given unit, put into "output" <pre> area
 function showJournal(unitName, filter_arg) {
@@ -83,9 +83,8 @@ cockpit.transport.wait(() => {
         else if (process.state === ProcessState.FAILED)
             process.reset();
         else
-            // Use stdbuf to force line buffering for both stdout and stderr to ensure output appears immediately
             if("" != command.value) {
-                process.run(["/bin/sh", "-ec", command.value])
+                process.run(["stdbuf", "-oL", "-eL", command.value])
                         .catch(ex => {
                             state.textContent = "Error: " + ex.toString();
                             run_button.setAttribute("disabled", "");
